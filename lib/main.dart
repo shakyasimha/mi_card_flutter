80aba3dffd56fc45e9a7535f5c63d0297826bc60
Sushovan Shakya <47445119+steinderace@users.noreply.github.com>
2022-02-11 13:46:00 +0545

    many changes to the code

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.teal.shade600,
         body: SafeArea(
-          child: Column(children: <Widget>[
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+              Widget>[
             CircleAvatar(
-              radius: 50.0,
-              //backgroundColor: Colors.red,
+              radius: 60.0,
               backgroundImage: AssetImage('images/profile_pic.jpg'),
             ),
             Text(
@@ -38,21 +38,25 @@ class MyApp extends StatelessWidget {
                 color: Colors.teal.shade100,
               ),
             ),
-            Container(
+            SizedBox(
+                height: 50.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.white,
+                )),
+            Card(
               color: Colors.white,
               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
+              //padding: EdgeInsets.all(10.0),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
                     Icons.phone,
                     size: 20.0,
                     color: Colors.teal.shade300,
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
+                  title: Text(
                     '+977 9861804526',
                     style: TextStyle(
                       fontFamily: 'Source Sans Pro',
@@ -60,31 +64,29 @@ class MyApp extends StatelessWidget {
                       color: Colors.teal.shade900,
                     ),
                   ),
-                ],
+                ),
               ),
             ),
-            Container(
+            Card(
               color: Colors.white,
               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
+              //padding: EdgeInsets.all(10.0),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
                     Icons.email,
                     color: Colors.teal.shade300,
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
+                  title: Text(
                     'sushakya235@gmail.com',
                     style: TextStyle(
                       fontFamily: 'Source Sans Pro',
                       fontSize: 20.0,
                       color: Colors.teal.shade900,
                     ),
-                  )
-                ],
+                  ),
+                ),
               ),
             ),
           ]),
