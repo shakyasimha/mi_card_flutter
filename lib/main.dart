09034964be5931ca69619fb66134c0b864747faa
Sushovan Shakya <47445119+steinderace@users.noreply.github.com>
2022-02-11 10:35:57 +0545

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,95 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              //backgroundColor: Colors.red,
+              backgroundImage: AssetImage('images/profile_pic.jpg'),
+            ),
+            Text(
+              'Sushovan Shakya',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontWeight: FontWeight.bold,
+                fontSize: 40.0,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              'Full Stack Developer',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+                letterSpacing: 2,
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    size: 20.0,
+                    color: Colors.teal.shade300,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    '+977 9861804526',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal.shade300,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'sushakya235@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ]),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
